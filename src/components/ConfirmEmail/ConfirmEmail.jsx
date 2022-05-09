f54a829a37d30c364514a72cf69eb53da57ca443
Mateusz Błażków <mateuszblazkow1@gmail.com>
2022-05-09 16:02:16 +0200

    Working email confirmations

--- src/components/ConfirmEmail/ConfirmEmail.jsx
+++ src/components/ConfirmEmail/ConfirmEmail.jsx
@@ -6,23 +6,30 @@ import APIAddress from "../../APIAddress";
 import axios from "../../services/api-interceptor";
 
 const ConfirmEmail = () =>{
+  const navigate = useNavigate();
     const {id} = useParams();
     const {token} = useParams();
     useEffect(() => {
         const fetchData = async () => {
           try {
             console.log(id, token);
+            let newToken = token.replace(/\//g, '%2f');
+            newToken = newToken.replace(/\+/g, '%2b');
+            newToken = newToken.replace(/\=/g, '%3d');
             let confirmMailDTO = {
               userId: id,
-              confirmationToken: token 
+              confirmationToken: newToken 
             };
+            console.log(confirmMailDTO);
             const response = await axios
               .post(APIAddress.value + "/api/User/ConfirmMail/",confirmMailDTO)
               .then(function (response) {
-                console.log(response.data);
+                toast.success("Konto zostało zatwierdzone!");
+                navigate("/login");
               });
           } catch (err) {
             toast.error(err.response.data.message);
+            console.log(err);
           }
         };
         fetchData();
